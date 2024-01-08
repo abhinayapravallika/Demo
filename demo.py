bdd65573647bd24fd270513389cd5293696361d6
RAMENA ABHINAYA PRAVALLIKA <147700710+abhinayapravallika@users.noreply.github.com>
2024-01-08 12:34:32 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.set_page_config(page_title="sparrows")
 st.header("Types of sparrows")
-col1,col2,col3,col4=st.columns(3)
+col1,col2,col3=st.columns(3)
 with col1:
   st.subheader("Song sparrow")
   st.image("https://www.allaboutbirds.org/guide/assets/photo/308771371-480px.jpg")
