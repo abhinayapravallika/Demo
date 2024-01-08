2fa5ad998cf41e37bd08ad812de19c7ccdbe049f
RAMENA ABHINAYA PRAVALLIKA <147700710+abhinayapravallika@users.noreply.github.com>
2024-01-08 12:00:32 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,9 +4,9 @@ st.header("Types of sparrows")
 col1,col2=st.columns(2)
 with col1:
   st.subheader("Song sparrow")
-  st.image("/.Song sparrow.img",caption="Song sparrow",width=300,use_column_width=True")
+  st.image("https://www.allaboutbirds.org/guide/assets/photo/308771371-480px.jpg")
   st.write("Song sparrows sing well.")
-with col1:
+with col2:
   st.subheader("House sparrow")
-  st.image("/.House sparrow.img",caption="Song sparrow",width=300,use_column_width=True")
+  st.image("https://wildlifesos.org/wp-content/uploads/2022/03/House-sparrow-female-scaled.jpg")
   st.write("House sparrows are good at adapting to urban environments.")
